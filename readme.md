717ef7730214a06a8b935f9ea2f4b1d40b1f82c7
Tahera <taherasaman@gmail.com>
2021-05-07 11:49:18 +0530

    setup production webpack build

---
+++ readme.md
@@ -0,0 +1,6 @@
+#Git
+
+init- create a new repository
+status- view changes to your project code
+add-add files to staging area 
+commit- greate a new commit with files from staging area
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const path = require('path')
-module.exports = {
-  entry: './src/app.js',
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = (env) => {
+  const isProduction = env ==='production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
@@ -12,16 +16,31 @@ module.exports = {
       exclude: /node_modules/
     },{
       test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
+      use: CSSExtract.extract({
+        use: [
+         {
+           loader: 'css-loader',
+           options: {
+             sourceMap: true
+           }
+         },
+         {
+           loader: 'sass-loader',
+           options: {
+             sourceMap: true
+           }
+         }
+        ]
+      })
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    CSSExtract
+  ],
+  devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true
+  }
   }
 };
